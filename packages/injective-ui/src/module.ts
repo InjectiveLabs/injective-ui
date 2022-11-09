66d21f583d7394873f7e2e865d927d14f5870e95
thomasRalee <thomas.leera@gmail.com>
2022-11-09 16:16:53 +0800

    fix: css import error

--- packages/injective-ui/src/module.ts
+++ packages/injective-ui/src/module.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath } from 'node:url'
 import { defineNuxtModule } from '@nuxt/kit'
 
-export * from './../lib/types'
-
 export default defineNuxtModule({
   meta: {
     name: 'base-components',
     configKey: 'baseComponents'
+  },
+  setup(_, nuxt) {
+    nuxt.options.css.push('@injectivelabs/injective-ui/lib/tailwind.css')
   },
   hooks: {
     'components:dirs'(dirs) {
