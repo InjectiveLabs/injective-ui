562c82f6414c4f38948f665e66da12a02ddef77d
thomasRalee <thomas.leera@gmail.com>
2022-11-23 18:33:11 +0800

    chore: remove tailwind eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -11,7 +11,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
-    'plugin:tailwindcss/recommended',
     'plugin:vue/vue3-recommended'
   ],
   parserOptions: {
@@ -21,14 +20,14 @@ module.exports = {
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
-    'tailwindcss/migration-from-tailwind-2': 'off',
-    'tailwindcss/no-custom-classname': 'off',
     'vue/html-self-closing': 'off',
     'vue/multi-word-component-names': 'off',
     'vue/no-multiple-template-root': 'off',
     'vue/no-v-model-argument': 'off',
     'vue/singleline-html-element-content-newline': 'off',
     'vue/v-on-event-hyphenation': 'off',
+    'vue/html-indent': 'off',
+    'vue/max-attributes-per-line': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
