c2571ebf3bad7be21f099f68bea4a1e928cc938a
thomasRalee <thomas.leera@gmail.com>
2022-11-24 13:20:55 +0800

    chore: package bump

--- packages/injective-ui/playground/nuxt.config.ts
+++ packages/injective-ui/playground/nuxt.config.ts
@@ -2,8 +2,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 import AppComponents from '..'
 
 export default defineNuxtConfig({
-  buildModules: ['@injectivelabs/ui-notifications'],
-  modules: [AppComponents, '@vueuse/nuxt', '@nuxtjs/tailwindcss'],
+  modules: [
+    AppComponents,
+    '@injectivelabs/ui-notifications',
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss'
+  ],
   tailwindcss: {
     cssPath: './../lib/css/tailwind.css'
   }
