565c074220572d0f4cb0fab7bb8f61edc1f1eed5
Ivan Angelkoski <56976418+nightexgv@users.noreply.github.com>
2023-04-18 03:24:59 +0200

    feat: checkbox

---
+++ packages/ui-shared/lib/components/Checkbox.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: Boolean,
+  checkboxOnRight: Boolean,
+  sm: Boolean,
+  lg: Boolean
+})
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: Boolean)
+}>()
+
+const isChecked = computed({
+  get: () => props.modelValue,
+  set: (value) => emits('update:modelValue', value)
+})
+
+const classesComputed = computed(() => {
+  if (props.sm) return 'w-3 h-3 relative'
+
+  if (props.lg) return 'w-5 h-5 relative'
+
+  return 'w-4 h-4 relative'
+})
+
+const checkboxClassesComputed = computed(() => {
+  if (props.sm)
+    return 'left-1/2 top-0 bottom-0 right-0 -translate-x-[2px] -translate-y-[1px] scale-75'
+
+  if (props.lg)
+    return 'left-1/2 top-0 bottom-0 right-0 -translate-x-1 -translate-y-[2px] scale-75'
+
+  return 'left-1/2 top-0 bottom-0 right-0 -translate-x-1 -translate-y-[2px] scale-75'
+})
+</script>
+
+<template>
+  <label class="flex space-x-2 relative">
+    <slot v-if="!checkboxOnRight" name="checkbox" v-bind="{ isChecked }">
+      <div class="grid place-items-center">
+        <div :class="classesComputed">
+          <div class="absolute inset-0 border-black dark:border-white border">
+            <div
+              v-if="isChecked"
+              class="absolute border-transparent dark:border-r-white dark:border-b-white border-r-black border-b-black border-2 rotate-45"
+              :class="checkboxClassesComputed"
+            />
+          </div>
+        </div>
+      </div>
+    </slot>
+
+    <div class="flex items-center">
+      <slot />
+    </div>
+
+    <slot v-if="checkboxOnRight" name="checkbox" v-bind="{ isChecked }">
+      <div class="grid place-items-center">
+        <div :class="classesComputed">
+          <div class="absolute inset-0 border-black dark:border-white border">
+            <div
+              v-if="isChecked"
+              class="absolute left-1/2 top-0 bottom-0 right-0 border-transparent dark:border-r-white dark:border-b-white border-r-black border-b-black border-2 rotate-45 -translate-x-1 -translate-y-[2px] scale-75"
+            />
+          </div>
+        </div>
+      </div>
+    </slot>
+
+    <input
+      v-model="isChecked"
+      type="checkbox"
+      class="absolute inset-0 opacity-0"
+    />
+  </label>
+</template>
