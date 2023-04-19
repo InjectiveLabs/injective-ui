fd355c7eeda89b3b7e83562946766ee9449a8222
thomasRalee <thomas.leera@gmail.com>
2023-04-20 01:14:19 +0800

    chore: make checkbox component fully renderless

--- packages/ui-shared/lib/components/Checkbox.vue
+++ packages/ui-shared/lib/components/Checkbox.vue
@@ -2,21 +2,9 @@
 import { computed } from 'vue'
 
 const props = defineProps({
-  lg: Boolean,
-  sm: Boolean,
+  disabled: Boolean,
   modelValue: Boolean,
-  checkboxOnRight: Boolean,
-
-  checkColor: {
-    type: String,
-    default:
-      'dark:border-r-white dark:border-b-white border-r-black border-b-black'
-  },
-
-  borderColor: {
-    type: String,
-    default: 'border-black dark:border-white'
-  }
+  checkboxOnRight: Boolean
 })
 
 const emits = defineEmits<{
@@ -25,63 +13,30 @@ const emits = defineEmits<{
 
 const isChecked = computed({
   get: () => props.modelValue,
-  set: (value: boolean) => emits('update:modelValue', value)
-})
-
-const sizeClasses = computed(() => {
-  if (props.sm) {
-    return 'w-3 h-3'
+  set: (value: boolean) => {
+    if (!props.disabled) {
+      emits('update:modelValue', value)
+    }
   }
-
-  if (props.lg) {
-    return 'w-5 h-5'
-  }
-
-  return 'w-4 h-4'
-})
-
-const checkboxClasses = computed(() => {
-  if (props.sm) {
-    return '-translate-x-[2px] -translate-y-[1px]'
-  }
-
-  if (props.lg) {
-    return '-translate-x-1 -translate-y-[2px]'
-  }
-
-  return '-translate-x-1 -translate-y-[2px]'
 })
 </script>
 
 <template>
   <label class="relative">
-    <div class="flex" :class="{ 'flex-row-reverse': checkboxOnRight }">
-      <slot
-        name="checkbox"
-        v-bind="{ isChecked, sizeClasses, checkboxClasses }"
-      >
-        <div class="grid place-items-center p-2">
-          <div class="relative" :class="sizeClasses">
-            <div class="absolute inset-0 border" :class="borderColor">
-              <div
-                v-if="isChecked"
-                class="absolute left-1/2 top-0 bottom-0 right-0 border-transparent border-2 rotate-45 scale-75"
-                :class="[checkboxClasses, checkColor]"
-              />
-            </div>
-          </div>
-        </div>
-      </slot>
+    <div
+      class="flex gap-2"
+      :class="[
+        { 'flex-row-reverse': checkboxOnRight },
+        disabled ? 'cursor-not-allowed' : 'cursor-pointer'
+      ]"
+    >
+      <slot name="checkbox" v-bind="{ isChecked, disabled }" />
 
       <div class="flex items-center">
         <slot />
       </div>
     </div>
 
-    <input
-      v-model="isChecked"
-      type="checkbox"
-      class="absolute inset-0 opacity-0"
-    />
+    <input v-model="isChecked" type="checkbox" class="hidden" />
   </label>
 </template>
--- packages/ui-shared/playground/story/Checkbox.story.vue
+++ packages/ui-shared/playground/story/Checkbox.story.vue
@@ -6,26 +6,55 @@ const checked = ref(false)
   <Story title="checkbox">
     <div class="p-2 max-w-lg space-y-4 text-white">
       <div class="border-b border-white py-2">
-        <BaseCheckbox v-model="checked"> Default Checkbox </BaseCheckbox>
-      </div>
-
-      <div class="border-b border-white py-2">
-        <BaseCheckbox
-          v-model="checked"
-          sm
-          border-color="border-blue-600"
-          check-color="border-r-blue-600 border-b-blue-600"
-        >
-          <span class="text-sm">sm Checkbox</span>
+        <BaseCheckbox v-model="checked" lg>
+          <template #checkbox="{ isChecked }">
+            <div class="grid place-items-center">
+              <div class="relative w-4 h-4">
+                <div class="absolute inset-0 border border-white">
+                  <div
+                    v-if="isChecked"
+                    class="absolute left-1/2 top-0 bottom-0 right-0 border-transparent border-2 rotate-45 scale-75 border-r-white border-b-white -translate-x-1 -translate-y-[2px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </template>
+          <span>Checkbox</span>
         </BaseCheckbox>
       </div>
 
       <div class="border-b border-white py-2">
-        <BaseCheckbox v-model="checked" lg> lg Checkbox </BaseCheckbox>
+        <BaseCheckbox v-model="checked" lg disabled>
+          <template #checkbox="{ isChecked }">
+            <div class="grid place-items-center">
+              <div class="relative w-4 h-4">
+                <div class="absolute inset-0 border border-white">
+                  <div
+                    v-if="isChecked"
+                    class="absolute left-1/2 top-0 bottom-0 right-0 border-transparent border-2 rotate-45 scale-75 border-r-white border-b-white -translate-x-1 -translate-y-[2px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </template>
+          Disabled
+        </BaseCheckbox>
       </div>
 
       <div class="border-b border-white py-2 flex justify-end">
         <BaseCheckbox v-model="checked" checkbox-on-right>
+          <template #checkbox="{ isChecked }">
+            <div class="grid place-items-center">
+              <div class="relative w-4 h-4">
+                <div class="absolute inset-0 border border-white">
+                  <div
+                    v-if="isChecked"
+                    class="absolute left-1/2 top-0 bottom-0 right-0 border-transparent border-2 rotate-45 scale-75 border-r-white border-b-white -translate-x-1 -translate-y-[2px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </template>
           Right Side Checkbox
         </BaseCheckbox>
       </div>
@@ -46,16 +75,6 @@ const checked = ref(false)
           </template>
 
           Custom Checkbox
-        </BaseCheckbox>
-      </div>
-
-      <div>
-        <BaseCheckbox v-model="checked" lg>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
-          doloremque quidem esse illum tenetur. Consequuntur modi expedita qui!
-          Consequuntur, aliquid! Distinctio doloribus pariatur, harum assumenda
-          nobis, nostrum quasi fuga, accusamus veritatis dolor culpa ea debitis
-          impedit quam cumque tenetur commodi.
         </BaseCheckbox>
       </div>
     </div>
