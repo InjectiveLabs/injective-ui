0896eaffaf336d110d63eb374a347b4c28183370
thomasRalee <thomas.leera@gmail.com>
2023-07-17 15:56:26 +0800

    chore: add base tooltip component

---
+++ packages/ui-shared/playground/story/Tooltip.story.vue
@@ -0,0 +1,13 @@
+<template>
+  <Story title="Tooltip">
+    <div class="h-screen p-4">
+      <BaseTooltip class="inline-block">
+        <BaseIcon name="circle-info" class="min-w-4 w-4 text-gray-600" />
+
+        <template #content>
+          <div class="max-w-[240px] p-2 text-sm">Random tooltip string</div>
+        </template>
+      </BaseTooltip>
+    </div>
+  </Story>
+</template>
