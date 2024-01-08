9492289cc63f0ed1f81ae605d6bf6e8b68551262
thomasRalee <thomas.leera@gmail.com>
2024-01-09 03:42:49 +0800

    chore: add QR code component

---
+++ layer/components/QRCode.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useQRCode } from '@vueuse/integrations/useQRCode'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+const qrcode = useQRCode(props.text)
+</script>
+
+<template>
+  <img :src="qrcode" alt="QR Code" />
+</template>
