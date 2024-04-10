f94542701a6bd9e0b726aa585b5f9f814299544e
thomasRalee <thomas.leera@gmail.com>
2024-04-11 05:09:58 +0800

    chore: layer token typing

---
+++ layer/types/market.ts
@@ -0,0 +1,31 @@
+import { SpotMarket, PerpetualMarket } from '@injectivelabs/sdk-ts'
+import { TokenStatic } from '@injectivelabs/token-metadata'
+import { SharedMarketType } from './enum'
+
+export interface SharedUiSpotMarket
+  extends Omit<SpotMarket, 'quoteToken' | 'baseToken'> {
+  baseToken: TokenStatic
+  quoteToken: TokenStatic
+  slug: string
+  priceDecimals: number
+  quantityDecimals: number
+  priceTensMultiplier: number
+  quantityTensMultiplier: number
+  type: SharedMarketType
+  subType: SharedMarketType
+  upcoming?: boolean
+}
+
+export interface SharedUiDerivativeMarket
+  extends Omit<PerpetualMarket, 'quoteToken'> {
+  baseToken: TokenStatic
+  quoteToken: TokenStatic
+  slug: string
+  priceDecimals: number
+  quantityDecimals: number
+  priceTensMultiplier: number
+  quantityTensMultiplier: number
+  type: SharedMarketType
+  subType: SharedMarketType
+  upcoming?: boolean
+}
