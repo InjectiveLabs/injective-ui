7b5ef9ed2f2318c0b126f51bb3124f9f761659a1
shane-moore <skm1790@gmail.com>
2024-04-11 16:24:43 -0400

    chore: add base components

---
+++ layer/components/DropdownItem.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { PropType } from 'vue'
+import { BaseDropdownOption } from './../../lib/types'
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<BaseDropdownOption>,
+    required: true
+  }
+})
+
+const emit = defineEmits<{
+  'item:selected': [state: string]
+  'update:item': [state: string]
+}>()
+
+function onClick() {
+  emit('item:selected', props.item.value)
+  emit('update:item', props.item.value)
+}
+</script>
+
+<template>
+  <div class="flex cursor-pointer items-center" @click="onClick">
+    <slot :item="item" />
+  </div>
+</template>
