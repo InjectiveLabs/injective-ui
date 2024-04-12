ef79f3e40cbd0dc2154fa8b80e9d99d522023279
shane-moore <skm1790@gmail.com>
2024-04-12 17:36:17 -0400

    chore: abstract bridge network and channelIds

---
+++ layer/types/bridge.ts
@@ -0,0 +1,41 @@
+export type CosmosChannel = {
+  aChainId: string
+  aToBChannelId: string
+  aToBClientId: string
+  bToAClientId: string
+  bToAChannelId: string
+  bChainId: string
+  port: string
+}
+
+export enum Network {
+  Axelar = 'axelar',
+  Canto = 'canto',
+  Chihuahua = 'chihuahua',
+  CosmosHub = 'cosmosHub',
+  Ethereum = 'ethereum',
+  EthereumWh = 'ethereumWh',
+  Evmos = 'evmos',
+  Injective = 'injective',
+  Juno = 'juno',
+  Osmosis = 'osmosis',
+  Persistence = 'Persistence',
+  Terra = 'terra',
+  Secret = 'secret',
+  Stride = 'stride',
+  Crescent = 'crescent',
+  Solana = 'solana',
+  Sommelier = 'sommelier',
+  Arbitrum = 'arbitrum',
+  Polygon = 'polygon',
+  Klaytn = 'klaytn',
+  Sui = 'sui',
+  Kava = 'kava',
+  Oraichain = 'oraichain',
+  Noble = 'noble',
+  Celestia = 'celestia',
+  Migaloo = 'migaloo',
+  Kujira = 'kujira',
+  CosmosHubTestnet = 'cosmosHub-testnet',
+  Andromeda = 'andromeda'
+}
