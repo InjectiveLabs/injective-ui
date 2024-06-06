95ad047e428cd2800dbd5b1516db8a91fbc2545f
thomasRalee <thomas.leera@gmail.com>
2024-06-06 16:59:18 +0200

    chore: add components

---
+++ layer/components/Datepicker.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import Datepicker from '@vuepic/vue-datepicker'
+</script>
+
+<template>
+  <Datepicker v-bind="$attrs">
+    <template #input-icon>
+      <slot name="input-icon" />
+    </template>
+
+    <template #clear-icon>
+      <slot name="clear-icon" />
+    </template>
+  </Datepicker>
+</template>
