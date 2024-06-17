50fc95e768801717e2248d5b8887239bbb9cce5a
thomasRalee <thomas.leera@gmail.com>
2024-06-17 18:50:42 +0800

    fix: minor

--- layer/nuxt-config/vite/index.ts
+++ layer/nuxt-config/vite/index.ts
@@ -60,6 +60,7 @@ export default defineConfig({
       'floating-vue',
       'canvas-confetti',
       'lightweight-charts',
+      '@cosmjs/stargate',
       '@cosmjs/launchpad',
       '@injectivelabs/utils',
       '@injectivelabs/sdk-ts',
