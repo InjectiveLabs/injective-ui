d3ebc33c1e2cbc8fc16294db6eda03a8407331d5
shane-moore <skm1790@gmail.com>
2024-07-29 14:44:22 -0700

    fix: ibcApi getDenomTrace

--- layer/services/tokenClient.ts
+++ layer/services/tokenClient.ts
@@ -77,17 +77,13 @@ export class SharedTokenClient {
       }
     }
 
-    const path = data.trace
-      .map((trace) => `transfer/${trace.channelId}`)
-      .join('/')
-
     const token = {
       ...unknownToken,
       hash,
       denom,
-      path,
-      baseDenom: data.base,
-      channelId: data.trace[0].channelId
+      path: data.path,
+      baseDenom: data.baseDenom,
+      channelId: data.path.split('/').pop() as string
     }
 
     this.cachedTokens[denom.toLowerCase()] = token
