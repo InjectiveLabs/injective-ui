dfe115894182c15637fec3e7bea7fe3e2222d4f9
thomasRalee <thomas.leera@gmail.com>
2024-08-09 00:35:51 +0800

    chore: add explorer block type

--- layer/types/explorer.ts
+++ layer/types/explorer.ts
@@ -1,9 +1,13 @@
-import { Coin, ExplorerTransaction } from '@injectivelabs/sdk-ts'
+import { Coin, BlockWithTxs, ExplorerTransaction } from '@injectivelabs/sdk-ts'
 
 export interface UiExplorerTransaction extends ExplorerTransaction {
   types: string[]
   coinReceived: Coin[]
   coinSpent: Coin[]
+}
+
+export interface UiExplorerBlockWithTxs extends Omit<BlockWithTxs, 'txs'> {
+  txs: UiExplorerTransaction[]
 }
 
 export type SharedEventAttribute = {
