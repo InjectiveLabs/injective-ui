5ea918268fd054a2afe35215a2d0ee21f2486487
thomasRalee <thomas.leera@gmail.com>
2024-09-09 18:17:02 +0800

    fix: qrCode component default value

--- layer/components/QRCode.vue
+++ layer/components/QRCode.vue
@@ -8,12 +8,49 @@ withDefaults(
   }>(),
   {}
 )
+
+const option = {
+  value: '',
+  width: 300,
+  height: 300,
+  margin: 0,
+  imgclass: '',
+  myclass: '',
+  downloadButton: '',
+  ButtonName: 'Download',
+  qrOptions: {
+    typeNumber: 0,
+    mode: 'Byte',
+    errorCorrectionLevel: 'Q'
+  },
+  imageOptions: { hideBackgroundDots: true, imageSize: 0.4, margin: 0 },
+  dotsOptions: {
+    type: 'dots',
+    color: '#6a1a4c',
+    gradient: {
+      type: 'linear',
+      rotation: 0,
+      colorStops: [
+        { offset: 0, color: '#6a1a4c' },
+        { offset: 1, color: '#6a1a4c' }
+      ]
+    }
+  },
+  backgroundOptions: { color: '#ffffff' },
+  cornersSquareOptions: { type: 'dot', color: '#000000' },
+  cornersDotOptions: { type: undefined, color: '#000000' },
+  fileExt: 'png',
+  image: '',
+  download: false,
+  downloadOptions: { name: 'vqr', extension: 'png' }
+}
 </script>
 
 <template>
   <div class="p-2 bg-white">
     <QRCodeVue3
       v-bind="{
+        ...option,
         value: text,
         image: injLogoBase64,
         margin: 0,
