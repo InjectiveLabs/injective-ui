a5a4495f81b10f9b2e26454645eec65d3a1ddf8a
thomasRalee <thomas.leera@gmail.com>
2025-01-18 03:02:11 +0800

    chore: add remaining msgSummaries

--- layer/transformer/explorer/messageSummary.ts
+++ layer/transformer/explorer/messageSummary.ts
@@ -1,10 +1,31 @@
-import { type Message } from '@injectivelabs/sdk-ts'
+import { type Coin, type Message } from '@injectivelabs/sdk-ts'
 import { MsgType } from '@injectivelabs/ts-types'
 import { getNetworkFromAddress } from './../../utils/network'
+
+const AUCTION_POOL_SUBACCOUNT_ID =
+  '0x1111111111111111111111111111111111111111111111111111111111111111'
 
 const exchangeMsgSummaryMap: Partial<
   Record<MsgType, (value: Message) => string[]>
 > = {
+  [MsgType.MsgWithdraw]: (value: Message) => {
+    const {
+      sender,
+      amount: { denom, amount },
+      subaccount_id: subaccountId
+    } = value.message
+
+    return [
+      `{{account:${sender}}} withdrew {{denom:${denom}-${amount}}} from subaccount {{subaccount:${subaccountId}}}`
+    ]
+  },
+
+  [MsgType.MsgInstantSpotMarketLaunch]: (value: Message) => {
+    const { sender, ticker } = value.message
+
+    return [`{{account:${sender}}} instant launched the ${ticker} Spot Market`]
+  },
+
   [MsgType.MsgCreateSpotMarketOrder]: (value: Message) => {
     const { sender, order } = value.message
 
@@ -186,6 +207,32 @@ const exchangeMsgSummaryMap: Partial<
       ...spotCancelOrders,
       ...derivativeCancelOrders
     ]
+  },
+
+  [MsgType.MsgIncreasePositionMargin]: (value: Message) => {
+    const {
+      sender,
+      amount,
+      market_id: marketId,
+      source_subaccount_id: sourceSubaccountId,
+      destination_subaccount_id: destinationSubaccountId
+    } = value.message
+
+    return [
+      `{{account:${sender}}} increased position margin by ${amount} for the {{market:${marketId}}} from subaccount {{subaccount:${sourceSubaccountId}}} to subaccount {{subaccount:${destinationSubaccountId}}}`
+    ]
+  },
+
+  [MsgType.MsgLiquidatePosition]: (value: Message) => {
+    const {
+      sender,
+      market_id: marketId,
+      subaccount_id: subaccountId
+    } = value.message
+
+    return [
+      `{{account:${sender}}} liquidated a position in the {{market:${marketId}}} market that belonged to the subaccount {{subaccount:${subaccountId}}}`
+    ]
   }
 }
 
@@ -204,6 +251,34 @@ const stakingMsgSummaryMap: Partial<
     ]
   },
 
+  [MsgType.MsgUnjail]: (value: Message) => {
+    const { validator_addr: validatorAddress } = value.message
+
+    return [`{{validator:${validatorAddress}}} sent an unjail message`]
+  },
+
+  [MsgType.MsgCreateValidator]: (value: Message) => {
+    const {
+      description: { moniker },
+      validator_address: validatorAddress
+    } = value.message
+
+    return [
+      `Validator ${moniker} has been created with the address {{account:${validatorAddress}}}`
+    ]
+  },
+
+  [MsgType.MsgEditValidator]: (value: Message) => {
+    const {
+      description: { moniker },
+      validator_address: validatorAddress
+    } = value.message
+
+    return [
+      `{{validator:${validatorAddress}}} modified ${moniker} validator details`
+    ]
+  },
+
   [MsgType.MsgBeginRedelegate]: (value: Message) => {
     const {
       amount: { denom, amount },
@@ -217,6 +292,17 @@ const stakingMsgSummaryMap: Partial<
     ]
   },
 
+  [MsgType.MsgWithdrawDelegatorReward]: (value: Message) => {
+    const {
+      delegator_address: delegatorAddress,
+      validator_address: validatorAddress
+    } = value.message
+
+    return [
+      `{{account:${delegatorAddress}}} claimed rewards from {{validator:${validatorAddress}}}`
+    ]
+  },
+
   [MsgType.MsgUndelegate]: (value: Message) => {
     const {
       amount: { denom, amount },
@@ -225,16 +311,7 @@ const stakingMsgSummaryMap: Partial<
     } = value.message
 
     return [
-      `{{account:${delegator}}} undelegated {{denom:${denom}-${amount}}} from {{validator:${validator}}}`
-    ]
-  },
-
-  [MsgType.MsgWithdrawDelegatorReward]: (value: Message) => {
-    const { delegator_address: delegator, validator_address: validator } =
-      value.message
-
-    return [
-      `{{account:${delegator}}} claimed rewards from {{validator:${validator}}}`
+      `{{account:${delegator}}} unstaked {{denom:${denom}-${amount}}} from {{validator:${validator}}}`
     ]
   }
 }
@@ -242,6 +319,17 @@ const stakingMsgSummaryMap: Partial<
 const insuranceMsgSummaryMap: Partial<
   Record<MsgType, (value: Message) => string[]>
 > = {
+  [MsgType.MsgCreateInsuranceFund]: (value: Message) => {
+    const {
+      sender,
+      ticker,
+      initial_deposit: { amount, denom }
+    } = value.message
+
+    return [
+      `{{account:${sender}}} created an insurance fund with an initial deposit of {{denom:${denom}-${amount}}} for the ${ticker} market`
+    ]
+  },
   [MsgType.MsgRequestRedemption]: (value: Message) => {
     const {
       sender,
@@ -252,10 +340,104 @@ const insuranceMsgSummaryMap: Partial<
     return [
       `{{account:${sender}}} requested a redemption of {{denom:${denom}-${amount}}} from the {{market:${marketId}}} Insurance Fund`
     ]
+  },
+  [MsgType.MsgUnderwrite]: (value: Message) => {
+    const {
+      sender,
+      market_id: marketId,
+      deposit: { amount, denom }
+    } = value.message
+
+    return [
+      `{{account:${sender}}} underwrote {{denom:${denom}-${amount}}} in the {{market:${marketId}}} insurance fund`
+    ]
   }
 }
 
+const peggyMsgSummaryMap: Partial<
+  Record<MsgType, (value: Message) => string[]>
+> = {
+  [MsgType.MsgConfirmBatch]: (value: Message) => {
+    const { orchestrator } = value.message
+
+    return [`${orchestrator} confirmed a batch request`]
+  },
+  [MsgType.MsgRequestBatch]: (value: Message) => {
+    const { orchestrator } = value.message
+
+    return [`${orchestrator} sent a batch request`]
+  },
+  [MsgType.MsgValsetConfirm]: (value: Message) => {
+    const { orchestrator } = value.message
+
+    return [`${orchestrator} confirmed the Valset`]
+  },
+  [MsgType.MsgSetOrchestratorAddresses]: (value: Message) => {
+    const { sender, orchestrator } = value.message
+
+    return [
+      `{{account:${sender}}} set the orchestrator address to {{account:${orchestrator}}}`
+    ]
+  },
+  [MsgType.MsgSendToEth]: (value: Message) => {
+    const { amount, sender, eth_dest: receiver } = value.message
+
+    return [
+      `{{account:${sender}}} withdrew {{denom:${amount.denom}-${amount.amount}}} to {{externalAccount:${receiver}}} on Ethereum`
+    ]
+  }
+}
+
+const govMsgSummaryMap: Partial<Record<MsgType, (value: Message) => string[]>> =
+  {
+    [MsgType.MsgDepositCosmos]: (value: Message) => {
+      const { amount, depositor, proposal_id: proposalId } = value.message
+
+      const [coin] = amount
+
+      return [
+        `{{account:${depositor}}} deposited {{denom:${coin.denom}-${coin.amount}}} to proposal {{proposal:${proposalId}}}`
+      ]
+    },
+    [MsgType.MsgVote]: (value: Message) => {
+      const {
+        voter,
+        option: optionRaw,
+        proposal_id: proposalId
+      } = value.message
+
+      let option = 'noWithVeto'
+
+      if (optionRaw === 'VOTE_OPTION_YES') {
+        option = 'yes'
+      }
+
+      if (optionRaw === 'VOTE_OPTION_ABSTAIN') {
+        option = 'abstain'
+      }
+
+      if (optionRaw === 'VOTE_OPTION_NO') {
+        option = 'no'
+      }
+
+      return [
+        `{{account:${voter}}} voted ${option} for {{proposal:${proposalId}}}`
+      ]
+    },
+    [MsgType.MsgSubmitProposal]: (value: Message) => {
+      const { proposer, initial_deposit: amount } = value.message
+
+      const [coin] = amount
+
+      return [
+        `{{account:${proposer}}} submitted a proposal with an initial deposit of {{denom:${coin.denom}-${coin.amount}}}`
+      ]
+    }
+  }
+
 const msgSummaryMap: Partial<Record<MsgType, (value: Message) => string[]>> = {
+  ...govMsgSummaryMap,
+  ...peggyMsgSummaryMap,
   ...stakingMsgSummaryMap,
   ...exchangeMsgSummaryMap,
   ...insuranceMsgSummaryMap,
@@ -266,6 +448,64 @@ const msgSummaryMap: Partial<Record<MsgType, (value: Message) => string[]>> = {
 
     return [
       `{{account:${sender}}} sent {{denom:${coin.denom}-${coin.amount}}} to {{account:${receiver}}}`
+    ]
+  },
+
+  [MsgType.MsgMultiSend]: (value: Message) => {
+    const { inputs, outputs } = value.message
+
+    return [
+      ...inputs.map(
+        (sender: { address: string; coins: Coin[] }) =>
+          `{{account:${sender.address}}} ${sender.coins
+            .map(({ denom, amount }) => `sent {{denom:${denom}-${amount}}}`)
+            .join(', ')}`
+      ),
+      ...outputs.map(
+        (sender: { address: string; coins: Coin[] }) =>
+          `{{account:${sender.address}}} ${sender.coins
+            .map(({ denom, amount }) => `received {{denom:${denom}-${amount}}}`)
+            .join(', ')}`
+      )
+    ]
+  },
+
+  [MsgType.MsgRecvPacket]: (value: Message) => {
+    const { packet } = value.message
+    const decodedPacketData = JSON.parse(
+      Buffer.from(packet.data, 'base64').toString('utf-8')
+    )
+
+    const { amount, denom, sender, receiver } = decodedPacketData
+
+    if (!amount && !denom && !sender && !receiver) {
+      return []
+    }
+
+    const injNetworkDenom = denom.split('/').pop()
+
+    return [
+      `{{externalAccount:${sender}}} deposited {{denom:${injNetworkDenom}-${amount}}} to {{account:${receiver}}} from ${getNetworkFromAddress(
+        sender
+      )}`
+    ]
+  },
+
+  [MsgType.MsgExternalTransfer]: (value: Message) => {
+    const {
+      sender,
+      amount: { denom, amount },
+      source_subaccount_id: sourceSubaccountId,
+      destination_subaccount_id: destinationSubaccountId
+    } = value.message
+
+    const suffix =
+      destinationSubaccountId === AUCTION_POOL_SUBACCOUNT_ID
+        ? ' as a contribution to the next auction pool'
+        : ''
+
+    return [
+      `{{account:${sender}}} transferred {{denom:${denom}-${amount}}} from {{subaccount:${sourceSubaccountId}}} to subaccount {{subaccount:${destinationSubaccountId}}}${suffix}`
     ]
   },
 
@@ -290,7 +530,7 @@ const msgSummaryMap: Partial<Record<MsgType, (value: Message) => string[]>> = {
     } = value.message
 
     return [
-      `{{cosmosAccount:${sender}}} deposited {{denom:${denom}-${amount}}} to {{account:${receiver}}} on Injective`
+      `{{externalAccount:${sender}}} deposited {{denom:${denom}-${amount}}} to {{account:${receiver}}} on Injective`
     ]
   },
 
@@ -336,18 +576,6 @@ const msgSummaryMap: Partial<Record<MsgType, (value: Message) => string[]>> = {
     return [
       `{{account:${sender}}} transferred {{denom:${denom}-${amount}}} from subaccount {{subaccount:${sourceSubaccountId}}} to subaccount {{subaccount:${destinationSubaccountId}}}`
     ]
-  },
-
-  [MsgType.MsgWithdraw]: (value: Message) => {
-    const {
-      sender,
-      amount: { denom, amount },
-      subaccount_id: subaccountId
-    } = value.message
-
-    return [
-      `{{account:${sender}}} withdrew {{denom:${denom}-${amount}}} from subaccount {{subaccount:${subaccountId}}}`
-    ]
   }
 }
 
@@ -362,3 +590,6 @@ export const getHumanReadableMessage = (value: Message): string[] => {
 
   return []
 }
+
+// todo:
+// /ibc.core.channel.v1.MsgTimeout
