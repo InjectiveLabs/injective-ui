b003d57eb34751ce4949181671bbefd82dd106e7
thomasRalee <thomas.leera@gmail.com>
2025-01-21 01:55:34 +0800

    chore: add transaction events parser

---
+++ layer/types/enum/explorer.ts
@@ -0,0 +1,6 @@
+export enum EventMessageType {
+  CancelSpotOrder = 'cancelSpotOrder',
+  BatchCancelSpotOrders = 'batchCancelSpotOrders',
+  CancelDerivativeOrder = 'cancelDerivativeOrder',
+  BatchCancelDerivativeOrders = 'batchCancelDerivativeOrders'
+}
