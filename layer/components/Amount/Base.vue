c813adf283658a9929574527bd4ef90ecb3f44cf
0xA1337 <0xa1337@proton.me>
2025-07-01 12:03:28 +0200

    feat: prioritize smaller than over subscripts

--- layer/components/Amount/Base.vue
+++ layer/components/Amount/Base.vue
@@ -118,10 +118,10 @@ const formattedAmount = computed(() => {
     <slot name="prefix" />
 
     <span v-if="showZeroAsEmDash && amountToBigNumber.eq(0)"> &mdash; </span>
-    <span v-else-if="subscriptedAmount" v-html="subscriptedAmount"></span>
     <span v-else-if="shouldHaveSmallerThan">
       &lt;{{ minDecimalThreshold.toFormat() }}
     </span>
+    <span v-else-if="subscriptedAmount" v-html="subscriptedAmount"></span>
     <span v-else-if="abbreviatedAmount">
       {{ abbreviatedAmount }}
     </span>
