8f12192ffa2b3ded7f68ae87217f49018635fd28
frederick-88 <frederickfd88@gmail.com>
2025-07-18 23:51:54 +0700

    chore: add helix logo base64 + update QRcode to support customization for share pnl (IL-1718)

--- layer/components/QRCode.vue
+++ layer/components/QRCode.vue
@@ -5,11 +5,15 @@ import { injLogoBase64 } from './../data/token'
 withDefaults(
   defineProps<{
     text: string
-    color?: string
+    logo?: string
+    imageOptions?: object
     extraConfigs?: object
+    colorSettings?: object
   }>(),
   {
-    color: '',
+    logo: '',
+    imageOptions: undefined,
+    colorSettings: undefined,
     extraConfigs: () => ({})
   }
 )
@@ -19,25 +23,23 @@ withDefaults(
   <div class="p-2 bg-white">
     <QRCodeVue3
       v-bind="{
-        value: text,
-        image: injLogoBase64,
         margin: 0,
+        value: text,
+        image: logo || injLogoBase64,
         dotsOptions: {
           type: 'dots',
-          ...(color
-            ? { color }
-            : {
-                gradient: {
-                  type: 'radial',
-                  rotation: 0,
-                  colorStops: [
-                    { offset: 0.5, color: '#7C70FF' },
-                    { offset: 1, color: '#5208c7' }
-                  ]
-                }
-              })
+          ...(colorSettings || {
+            gradient: {
+              type: 'radial',
+              rotation: 0,
+              colorStops: [
+                { offset: 0.5, color: '#7C70FF' },
+                { offset: 1, color: '#5208c7' }
+              ]
+            }
+          })
         },
-        imageOptions: { imageSize: 0.4, margin: 16 },
+        imageOptions: imageOptions || { imageSize: 0.4, margin: 16 },
         cornersDotOptions: {
           type: 'dot',
           color: '#000000'
