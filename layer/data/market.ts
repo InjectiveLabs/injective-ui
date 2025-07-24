c429f1b54d1827df0e93c736b7cf20fdbe35e23d
thomasRalee <thomas.leera@gmail.com>
2025-07-24 22:15:22 +0800

    chore: unhide SBET/USDT Perp market

--- layer/data/market.ts
+++ layer/data/market.ts
@@ -1,3 +1,1 @@
-export const MARKET_IDS_TO_HIDE: string[] = [
-  '0xa64335c69ee025897d00880752971320c8bf35fa7346c58cd208bb8e996e95e3' // SBET-USDT-PERP
-]
+export const MARKET_IDS_TO_HIDE: string[] = []
