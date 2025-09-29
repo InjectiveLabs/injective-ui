24becd2dcd5459afa5963bca7295419142bf3da8
0xA1337 <0xa1337@proton.me>
2025-09-29 16:50:59 +0200

    chore: add percentage decimals constant

--- utils/constant/index.ts
+++ utils/constant/index.ts
@@ -47,9 +47,10 @@ export const MAX_TOAST_TIMEOUT = 10 * 24 * 60 * 60 * 1000 // 10 days
 
 export const NOTIFI_LINK = 'https://injective.notifi.network'
 
-export const DEFAULT_USD_DECIMALS = 2
+export const DEFAULT_USD_DECIMALS = 3
 export const DEFAULT_ASSET_DECIMALS = 6
 export const DEFAULT_DISPLAY_DECIMALS = 8
+export const DEFAULT_PERCENTAGE_DECIMALS = 2
 
 export const DEFAULT_ABBREVIATED_DECIMALS = 2
 export const DEFAULT_ABBREVIATION_THRESHOLD = 1_000_000
