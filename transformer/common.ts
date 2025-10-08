2ce63c14fe7b519407f00b24d92c58d5c7938440
thomasRalee <thomas.leera@gmail.com>
2025-10-08 23:51:58 +0800

    chore: add pageResponseToPagination helper function

---
+++ transformer/common.ts
@@ -0,0 +1,34 @@
+import { paginationUint8ArrayToString } from '@injectivelabs/sdk-ts'
+import type { Pagination, PagePagination } from '@injectivelabs/sdk-ts'
+
+export const pageResponseToPagination = ({
+  newPagination,
+  oldPagination
+}: {
+  newPagination?: undefined | Pagination
+  oldPagination: undefined | PagePagination
+}): PagePagination => {
+  if (!newPagination) {
+    return {
+      prev: null,
+      current: null,
+      next: null
+    }
+  }
+
+  const next = paginationUint8ArrayToString(newPagination.next)
+
+  if (!oldPagination) {
+    return {
+      prev: null,
+      current: null,
+      next
+    }
+  }
+
+  return {
+    prev: oldPagination.current,
+    current: oldPagination.next,
+    next
+  }
+}
--- transformer/oracle.ts
+++ transformer/oracle.ts
@@ -1,30 +1,31 @@
-import { OracleType, OracleTypeMap } from '@injectivelabs/sdk-ts'
+import { OracleTypeMap } from '@injectivelabs/sdk-ts'
+import type { OracleType} from '@injectivelabs/sdk-ts';
 
 export class UiOracleTransformer {
   static grpcOracleTypeToUiOracleType(grpcOracleType: string): OracleType {
     switch (grpcOracleType.toLowerCase()) {
-      case 'coinbase':
-        return OracleTypeMap.Coinbase
-      case 'bandibc':
-        return OracleTypeMap.BandIBC
       case 'pricefeed':
         return OracleTypeMap.PriceFeed
+      case 'chainlink':
+        return OracleTypeMap.Chainlink
+      case 'coinbase':
+        return OracleTypeMap.Coinbase
+      case 'provider':
+        return OracleTypeMap.Provider
+      case 'bandibc':
+        return OracleTypeMap.BandIBC
+      case 'razor':
+        return OracleTypeMap.Razor
       case 'band':
         return OracleTypeMap.Band
       case 'api3':
         return OracleTypeMap.API3
-      case 'razor':
-        return OracleTypeMap.Razor
+      case 'pyth':
+        return OracleTypeMap.Pyth
       case 'dia':
         return OracleTypeMap.Dia
       case 'uma':
         return OracleTypeMap.Uma
-      case 'chainlink':
-        return OracleTypeMap.Chainlink
-      case 'pyth':
-        return OracleTypeMap.Pyth
-      case 'provider':
-        return OracleTypeMap.Provider
       default:
         return OracleTypeMap.Coinbase
     }
