3960be39d0e700a9c3f9eb1920c86f2ad6278919
thomasRalee <thomas.leera@gmail.com>
2026-05-04 23:54:59 +0800

    fix: rabby wallet extension redirecting user to install page

--- app/composables/useSharedWalletOptions.ts
+++ app/composables/useSharedWalletOptions.ts
@@ -1,42 +1,51 @@
 import { Wallet } from '@injectivelabs/wallet-base'
+import { getWalletStrategy } from './../wallet/strategy'
 import { IS_HELIX, IS_DEVNET, IS_MAINNET } from '../utils/constant'
 import type { SharedWalletOption } from '../types'
 
 export function useSharedWalletOptions() {
+  const evmProviders = ref<Wallet[]>([])
   const sharedWalletStore = useSharedWalletStore()
 
-  const popularOptions = computed(
-    () =>
-      [
-        {
-          wallet: Wallet.Metamask,
-          downloadLink: !sharedWalletStore.metamaskInstalled
+  const popularOptions = computed(() => {
+    return [
+      {
+        wallet: Wallet.Metamask,
+        downloadLink:
+          !sharedWalletStore.metamaskInstalled &&
+          !evmProviders.value.includes(Wallet.Metamask)
             ? 'https://metamask.io/download'
             : undefined
-        },
-        {
-          wallet: Wallet.Keplr,
-          downloadLink: !sharedWalletStore.keplrInstalled
+      },
+      {
+        wallet: Wallet.Keplr,
+        downloadLink:
+          !sharedWalletStore.keplrInstalled &&
+          !evmProviders.value.includes(Wallet.Keplr)
             ? 'https://www.keplr.app/download'
             : undefined
-        },
-        {
-          wallet: Wallet.Rabby,
-          downloadLink: !sharedWalletStore.rabbyInstalled
+      },
+      {
+        wallet: Wallet.Rabby,
+        downloadLink:
+          !sharedWalletStore.rabbyInstalled &&
+          !evmProviders.value.includes(Wallet.Rabby)
             ? 'https://rabby.io/'
             : undefined
-        }
-      ].filter((option) => option) as SharedWalletOption[]
-  )
+      }
+    ].filter((option) => option) as SharedWalletOption[]
+  })
 
   const options = computed(
     () =>
       [
         {
           wallet: Wallet.Rainbow,
-          downloadLink: !sharedWalletStore.rainbowInstalled
-            ? 'https://rainbow.me/download'
-            : undefined
+          downloadLink:
+            !sharedWalletStore.rainbowInstalled &&
+            !evmProviders.value.includes(Wallet.Rainbow)
+              ? 'https://rainbow.me/download'
+              : undefined
         },
         {
           wallet: Wallet.Leap,
@@ -48,15 +57,19 @@ export function useSharedWalletOptions() {
           ? undefined
           : {
               wallet: Wallet.BitGet,
-              downloadLink: !sharedWalletStore.bitGetInstalled
-                ? 'https://web3.bitget.com/en/wallet-download'
-                : undefined
+              downloadLink:
+                !sharedWalletStore.bitGetInstalled &&
+                !evmProviders.value.includes(Wallet.BitGet)
+                  ? 'https://web3.bitget.com/en/wallet-download'
+                  : undefined
             },
         {
           wallet: Wallet.OkxWallet,
-          downloadLink: !sharedWalletStore.okxWalletInstalled
-            ? 'https://www.okx.com/web3'
-            : undefined
+          downloadLink:
+            !sharedWalletStore.okxWalletInstalled &&
+            !evmProviders.value.includes(Wallet.OkxWallet)
+              ? 'https://www.okx.com/web3'
+              : undefined
         },
         { wallet: Wallet.Ledger },
         { wallet: Wallet.TrezorBip32 },
@@ -84,6 +97,21 @@ export function useSharedWalletOptions() {
   )
 
   async function validateWalletExtensionInstalled() {
+    const walletStrategy = await getWalletStrategy()
+
+    // Get EVM providers from the strategy to determine which wallet extensions are installed on client's browser
+    const strategies = walletStrategy.strategies
+    const evmStrategy =
+      strategies[Wallet.Metamask] ||
+      strategies[Wallet.Rabby] ||
+      strategies[Wallet.Rainbow] ||
+      strategies[Wallet.KeplrEvm] ||
+      strategies[Wallet.OkxWallet]
+
+    evmProviders.value = Object.keys(
+      (evmStrategy as any)?.evmProviders || {}
+    ) as Wallet[]
+
     await Promise.all([
       sharedWalletStore.checkIsBitGetInstalled(),
       sharedWalletStore.checkIsRainbowInstalled(),
